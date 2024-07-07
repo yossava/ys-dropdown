589adf61603e455f1ce09ec62c4a7d0c04163a94
yossava adhi <yoss017@gmail.com>
2024-07-07 10:10:42 +0700

    update  default value

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,10 +4,43 @@ import ReactDOM from "react-dom";
 import Pill from "./Pill";
 
 const Dropdown = ({
-  options,
+  options = [
+    {
+      label: "Option 1",
+      value: "option1",
+    },
+    {
+      label: "Option 2 with icon",
+      value: "option2",
+    },
+    {
+      label: "Long Option 3",
+      value: "option3",
+    },
+    {
+      label: "Long Long Option 4",
+      value: "option4",
+    },
+    {
+      label: "Long Long Long Option 5",
+      value: "option5",
+    },
+    {
+      label: "Long Long Long Long Option 6",
+      value: "option6",
+    },
+    {
+      label: "Long Long Long Long Long Option 7",
+      value: "option7",
+    },
+    {
+      label: "Long Long Long Long Long Long Option 8",
+      value: "option8",
+    },
+  ],
   isMultiple = false,
   label = "Label",
-  usePortal = true,
+  usePortal = false,
   enableSearch = true,
   outlined = true,
   id = "",
@@ -207,8 +240,8 @@ Dropdown.propTypes = {
         .isRequired,
       label: PropTypes.string.isRequired,
     })
-  ).isRequired,
-  isMultiple: PropTypes.bool.isRequired,
+  ),
+  isMultiple: PropTypes.bool,
   usePortal: PropTypes.bool,
   enableSearch: PropTypes.bool,
   outlined: PropTypes.bool,
